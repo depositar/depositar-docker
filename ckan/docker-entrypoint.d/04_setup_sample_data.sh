e75b97a1bdbb4a3528a75fef8f272447583e835e
tychuang <33109379+tychuang1211@users.noreply.github.com>
2025-07-04 14:11:29 +0800

    Remove upload logo

--- ckan/docker-entrypoint.d/04_setup_sample_data.sh
+++ ckan/docker-entrypoint.d/04_setup_sample_data.sh
@@ -4,8 +4,7 @@ export PATH=$PATH:/srv/app/.local/bin
 
 cd /srv/app/sample-data
 
-ckanapi load organizations -I organizations.jsonl --upload-logo
-
+ckanapi load organizations -I organizations.jsonl
 ckanapi load datasets -I datasets.jsonl
 
 ckanapi batch -I upload-files.jsonl --local-files
