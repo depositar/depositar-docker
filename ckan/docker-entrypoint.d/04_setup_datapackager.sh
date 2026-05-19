b1c215af534a91fb436d7f7f25118167cd35d5d6
Sol Lee <u103133.u103135@gmail.com>
2026-05-19 17:33:34 +0800

    Fix admin name

--- ckan/docker-entrypoint.d/04_setup_datapackager.sh
+++ ckan/docker-entrypoint.d/04_setup_datapackager.sh
@@ -5,7 +5,7 @@ if [[ $CKAN__PLUGINS == *"datapackager"* ]]; then
    # Set API token if necessary
    if [ -z "$CKANEXT__DATAPACKAGER__API_TOKEN" ] ; then
       echo "Set up ckanext.datapackager.api_token in the CKAN config file"
-      ckan config-tool $CKAN_INI "ckanext.datapackager.api_token=$(ckan -c $CKAN_INI user token add ckan_admin datapackager | tail -n 1 | tr -d '\t')"
+      ckan config-tool $CKAN_INI "ckanext.datapackager.api_token=$(ckan -c $CKAN_INI user token add ckan_demo datapackager | tail -n 1 | tr -d '\t')"
    fi
 else
    echo "Not configuring Datapackager"
